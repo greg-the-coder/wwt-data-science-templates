93768218fcfc7458e809c8c81ca6735ae30d869e
greg-the-coder <greg@coder.com>
2026-02-05 15:32:39 -0600

    Update command to use coder_agent.dev.init_script

--- ml-training/main.tf
+++ ml-training/main.tf
@@ -485,7 +485,7 @@ resource "kubernetes_deployment" "main" {
           name              = "dev"
           image             = var.image
           image_pull_policy = "Always"
-          command           = ["sh", "-c", "echo 'insecure' > ~/.curlrc && ${coder_agent.main.init_script}"]
+          command           = ["sh", "-c", coder_agent.dev.init_script]
           security_context {
             run_as_user = "1000"
           }
